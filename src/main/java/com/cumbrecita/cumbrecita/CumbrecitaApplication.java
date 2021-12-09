c13db3df2d6ef5859a67e3c03e159e73224e83ed
manuqs33 <manuelquirogasoto@gmail.com>
2021-12-09 19:50:29 -0300

    Initial commit

---
+++ src/main/java/com/cumbrecita/cumbrecita/CumbrecitaApplication.java
@@ -0,0 +1,13 @@
+package com.cumbrecita.cumbrecita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CumbrecitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CumbrecitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumbrecita/cumbrecita/CumbrecitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumbrecita.cumbrecita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CumbrecitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
