8279b90347252426e6b0c563880b582d6b238d9b
Joaquin Vasquez <joacovas@hotmail.com>
2021-12-09 22:08:54 -0300

    Se arreglaron los espacios para evitar conflicto

--- src/test/java/com/cumbrecita/cumbrecita/CumbrecitaApplicationTests.java
+++ src/test/java/com/cumbrecita/cumbrecita/CumbrecitaApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CumbrecitaApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
-
+    //@Test
+    //void contextLoads() {
+    //}
 }
