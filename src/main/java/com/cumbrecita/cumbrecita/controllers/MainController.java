a726e28028fc2adfe3fe1909e0bd86e706673ffa
Joaquin Vasquez <joacovas@hotmail.com>
2021-12-13 21:49:43 -0300

    Arreglos + controller.
    Cambiados los atributos end y start por endDate y startDate consecuentemente.

---
+++ src/main/java/com/cumbrecita/cumbrecita/controllers/MainController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cumbrecita.cumbrecita.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+    
+}
