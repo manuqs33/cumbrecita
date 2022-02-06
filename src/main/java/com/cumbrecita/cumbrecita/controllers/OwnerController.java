aa4f28fa4f9c07b4248a94a61d68555d174ef2a2
manuqs33 <manuelquirogasoto@gmail.com>
2022-02-06 14:55:07 -0300

    comentar tickets

--- src/main/java/com/cumbrecita/cumbrecita/controllers/OwnerController.java
+++ src/main/java/com/cumbrecita/cumbrecita/controllers/OwnerController.java
@@ -43,60 +43,60 @@ public class OwnerController {
     @Autowired
     public LodgingRepository lR;
 
-    @GetMapping("/client/ticket/{id}")//para ver un ticket en especifico
-    public String viewTicket(@PathVariable("id") String id, ModelMap model, HttpSession session) {
-
-        Owner admin = (Owner) session.getAttribute("sessionOwner");
-        if (admin == null) {
-            return "redirect:/";
-        }
-
-        OwnerTicket ot = otr.getById(id);
-
-        if (ot != null) {
-            model.put("ticket", ot);
-        }
-
-        return "ticket.html";
-    }
-
-    @GetMapping("/owner/new-ticket")
-    public String newTicket(ModelMap model, HttpSession session) {
-        Owner owner = (Owner) session.getAttribute("sessionOwner");
-        if (owner == null) {
-            return "redirect:/";
-        }
-        List<Lodging> lods = lR.searchByOwner(owner.getId());
-
-        model.addAttribute("lods", lods);
-
-        return "newTicket.html";
-    }
-    
-    @GetMapping("/owner/mytickets")
-    public String myTickets(ModelMap model, HttpSession session) {
-        
-        Owner owner = (Owner) session.getAttribute("sessionOwner");
-        if (owner == null) {
-            return "redirect:/";
-        }
-        
-        List<OwnerTicket> mytickets = otr.showMyTickets(owner.getId());
-        
-        model.put("tickets", mytickets);
-        
-        return "mytickets.html";
-    }
-
-    @PostMapping("/owner/create-ticket")
-    public String createTicket(HttpSession session, ModelMap model, String subjet, String text, MultipartFile file, String idLodging) {
-
-        try {
-            otS.newTicket(lR.getById(idLodging), subjet, text, file);
-        } catch (ErrorService ex) {
-            Logger.getLogger(ClientController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return "ticket.html";
-    }
+//    @GetMapping("/client/ticket/{id}")//para ver un ticket en especifico
+//    public String viewTicket(@PathVariable("id") String id, ModelMap model, HttpSession session) {
+//
+//        Owner admin = (Owner) session.getAttribute("sessionOwner");
+//        if (admin == null) {
+//            return "redirect:/";
+//        }
+//
+//        OwnerTicket ot = otr.getById(id);
+//
+//        if (ot != null) {
+//            model.put("ticket", ot);
+//        }
+//
+//        return "ticket.html";
+//    }
+//
+//    @GetMapping("/owner/new-ticket")
+//    public String newTicket(ModelMap model, HttpSession session) {
+//        Owner owner = (Owner) session.getAttribute("sessionOwner");
+//        if (owner == null) {
+//            return "redirect:/";
+//        }
+//        List<Lodging> lods = lR.searchByOwner(owner.getId());
+//
+//        model.addAttribute("lods", lods);
+//
+//        return "newTicket.html";
+//    }
+//    
+//    @GetMapping("/owner/mytickets")
+//    public String myTickets(ModelMap model, HttpSession session) {
+//        
+//        Owner owner = (Owner) session.getAttribute("sessionOwner");
+//        if (owner == null) {
+//            return "redirect:/";
+//        }
+//        
+//        List<OwnerTicket> mytickets = otr.showMyTickets(owner.getId());
+//        
+//        model.put("tickets", mytickets);
+//        
+//        return "mytickets.html";
+//    }
+//
+//    @PostMapping("/owner/create-ticket")
+//    public String createTicket(HttpSession session, ModelMap model, String subjet, String text, MultipartFile file, String idLodging) {
+//
+//        try {
+//            otS.newTicket(lR.getById(idLodging), subjet, text, file);
+//        } catch (ErrorService ex) {
+//            Logger.getLogger(ClientController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        return "ticket.html";
+//    }
 }
