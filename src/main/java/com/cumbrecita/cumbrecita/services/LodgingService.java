d1a3746f6a89c7d73fd6bcefa2c49a3a7f0224b4
Francisco Fernandez <franciscofernandezb@outlook.com>
2022-02-07 18:30:16 -0300

    Update LodgingService.java

--- src/main/java/com/cumbrecita/cumbrecita/services/LodgingService.java
+++ src/main/java/com/cumbrecita/cumbrecita/services/LodgingService.java
@@ -31,9 +31,9 @@ public class LodgingService {
         lodging.setAddress(address);
 
         if (t == 0 || t < 1) {
-            lodging.setT(Type.house);
+            lodging.setT(Type.Casa);
         } else if (t == 1 || t > 0) {
-            lodging.setT(Type.room);
+            lodging.setT(Type.Habitacion);
         }
 
         lodging.setCapacity(capacity);
@@ -61,9 +61,9 @@ public class LodgingService {
             lodging.setName(name);
             lodging.setAddress(address);
             if (t == 0 || t < 1) {
-                lodging.setT(Type.house);
+                lodging.setT(Type.Casa);
             } else if (t == 1 || t > 0) {
-                lodging.setT(Type.room);
+                lodging.setT(Type.Habitacion);
             }
             lodging.setCapacity(capacity);
             lodging.setPricepernight(pricepernight);
